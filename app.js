c29069858366f010764b98ea10bb6a2bcef9cbd6
Fakhri Yusifli <fakhriyusifli@gmail.com>
2020-10-22 01:08:44 +0400

    hold button

--- app.js
+++ app.js
@@ -33,7 +33,30 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     }else{
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] += roundScore;
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    if(scores[activePlayer] >= 100){
+        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    }else{
+        nextPlayer();
+    }
+
+});
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
 
         document.getElementById('current-0').textContent = '0';
@@ -43,10 +66,6 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.querySelector('.player-1-panel').classList.toggle('active');
         
         document.querySelector('.dice').style.display = 'none';
-    }
-});
-
-
-
+}
 
 
