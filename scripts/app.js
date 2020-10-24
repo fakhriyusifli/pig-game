d357c524b5b9f902f1e87b5253af7454c082f9a8
Fakhri Yusifli <fakhriyusifli@gmail.com>
2020-10-24 12:49:47 +0400

    two dice feature

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,23 @@
-var scores, roundScore, activePlayer, gamePlaying, lastDice;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     if(gamePlaying){
-        var dice = Math.floor(Math.random() * 6) + 1;
-        var diceDOM = document.querySelector('.dice');
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'imgs/dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'imgs/dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'imgs/dice-' + dice2 + '.png';
 
-        if(dice === 6 && lastDice === 6){
-            scores[activePlayer] = 0;
-            document.querySelector('#score-' + activePlayer).textContent = '0';
-            nextPlayer();
-        }else if(dice !== 1){
-            roundScore += dice;
+        if(dice1 !== 1 && dice2 !== 1){
+            roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }else{
             nextPlayer();
         }
-        lastDice = dice;
     }
 });
 
@@ -40,7 +37,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
         if(scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -61,7 +59,8 @@ function nextPlayer(){
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
         
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -71,8 +70,9 @@ function init(){
     roundScore = 0;
     activePlayer = 0;
     gamePlaying = true;
-
-    document.querySelector('.dice').style.display = 'none';
+    
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
