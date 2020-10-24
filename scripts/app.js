6f0a286b1a06b71f5a15ad5d0791b7c0f3060309
Fakhri Yusifli <fakhriyusifli@gmail.com>
2020-10-24 12:31:50 +0400

    manual final score feature

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,3 @@
-/*
-The GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's 
-the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-*/
-
 var scores, roundScore, activePlayer, gamePlaying, lastDice;
 
 init();
@@ -39,7 +29,16 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         scores[activePlayer] += roundScore;
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
-        if(scores[activePlayer] >= 100){
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        if(input){
+            winningScore = input;
+        }else{
+            winningScore = 101;
+        }
+
+        if(scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
